cd892110af5465e08fe15822ac51a4ea08008e5f
Indu Bakshi <bakshi.webworld@gmail.com>
2020-09-30 16:53:38 +0530

    Updates

--- precache-manifest.8986bf0bac8a7b07f19ef222a1489f11.js
+++ precache-manifest.8986bf0bac8a7b07f19ef222a1489f11.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f79d48a9b838e97caf66497b63603adf",
+    "revision": "43b1984d676a7914e2aa9417da166421",
     "url": "/nwes/index.html"
   },
   {
-    "revision": "e961daf9a065ddba3e97",
+    "revision": "5bcb2f271897062ec156",
     "url": "/nwes/static/css/main.a5caab70.chunk.css"
   },
   {
-    "revision": "2aabe95ac9890d49d5cb",
-    "url": "/nwes/static/js/2.18f99457.chunk.js"
+    "revision": "b8cc7550384ac4c07059",
+    "url": "/nwes/static/js/2.f131f7e0.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/nwes/static/js/2.18f99457.chunk.js.LICENSE.txt"
+    "url": "/nwes/static/js/2.f131f7e0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e961daf9a065ddba3e97",
-    "url": "/nwes/static/js/main.97321c63.chunk.js"
+    "revision": "5bcb2f271897062ec156",
+    "url": "/nwes/static/js/main.ab5e01d6.chunk.js"
   },
   {
     "revision": "97f7c5c63127290951cc",
